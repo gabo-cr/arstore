ef5001f73e6e2437ddde3f653382970a5c5fa43d
gabriel-labitcoinera <gabrielchavezriera@gmail.com>
2023-03-03 19:16:57 -0600

    Add Catalogo webhooks

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.catalogo, name='catalogo'),
+    path('webhook/new', views.webhookProduct, name='webhookProductNew'), #/catalogo/webhook/new
+    path('webhook/update', views.webhookProduct, name='webhookProductUpdate'), #/catalogo/webhook/update
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,10 +1,11 @@
-import os, environ
+import os, environ, base64, hashlib, hmac, json
 from pathlib import Path
 from django.shortcuts import render
 import shopify
 from .models import Articulo, LogArticulo
 from datetime import datetime
-import json
+from django.http import HttpResponse, HttpResponseBadRequest 
+from django.views.decorators.csrf import csrf_exempt
 
 #environ init
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 def catalogo(request):
     template = 'catalogo/catalogo.html'
-    
+
     numArticulosDB = Articulo.objects.count()
     numArticulosShopify = countProductsFromShopify()
     if numArticulosDB < numArticulosShopify:
@@ -26,6 +27,26 @@ def catalogo(request):
     }
     return render(request, template, context)
 
+@csrf_exempt
+def webhookProduct(request):
+    data = request.body
+    verified = verifyWebhook(data, request.headers['X-Shopify-Hmac-SHA256'])
+    if not verified:
+        return HttpResponseBadRequest()
+
+    #Handle webhook json response
+    #print(json.loads(data))
+    saveProductToDB(json.loads(data))
+
+    return HttpResponse()
+
+def verifyWebhook(data, hmac_header):
+    digest = hmac.new(env.str('CLIENT_SECRET').encode('utf-8'), data, digestmod=hashlib.sha256).digest()
+    computed_hmac = base64.b64encode(digest)
+
+    return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
+
+
 def loadAllArticulosFromDB():
     articulos = Articulo.objects.all()
     return articulos
@@ -37,31 +58,57 @@ def loadAllArticulosFromShopifyToDB():
         products = shopify.Product.find()
         productsDict = [p.to_dict() for p in products]
         for p in productsDict:
-            imageurl = None if p['image'] is None else p['image']['src']
-            if 'variants' in p:
-                for variant in p['variants']:
-                    articulo, created = Articulo.objects.update_or_create(
-                        sku=variant['sku'], 
-                        imagenURI=imageurl,
-                        nombre=p['title'] + ' ' + variant['title'],
-                        cantidad=variant['inventory_quantity'],
-                        fechaRegistro=datetime.fromisoformat(variant['created_at']),
-                        ultimaFechaActualizacion=datetime.fromisoformat(variant['updated_at']),
-                        product_id=variant['id']
-                    )
-                    if created:
-                        articulo.save()
-                        log = LogArticulo.objects.create(
-                            articulo=articulo,
-                            json=json.dumps({'articulo': variant})
-                        )
-                        log.save()
+            saveProductToDB(p)
     except Exception as e:
         print(e)
         return False
     finally:
         closeConnectionToShopify()
 
+    return True
+
+def saveProductToDB(product):
+    try:
+        imageurl = None if product['image'] is None else product['image']['src']
+        if 'variants' in product:
+            for variant in product['variants']:
+                articulos = Articulo.objects.filter(product_id=variant['id'])
+                print(articulos)
+                if len(articulos) == 0:
+                    articulo, created = Articulo.objects.create(
+                        sku = variant['sku'], 
+                        imagenURI = imageurl,
+                        nombre = product['title'] + ' ' + variant['title'],
+                        cantidad = variant['inventory_quantity'],
+                        fechaRegistro = datetime.fromisoformat(variant['created_at']),
+                        ultimaFechaActualizacion = datetime.fromisoformat(variant['updated_at']),
+                        product_id = variant['id']
+                    )
+                    if created:
+                        articulo.save()
+                        log = LogArticulo.objects.create(
+                            articulo = articulo,
+                            json = json.dumps({'articulo': variant})
+                        )
+                        log.save()
+                else:
+                    articulos[0].sku = variant['sku']
+                    articulos[0].imagenURI = imageurl
+                    articulos[0].nombre = product['title'] + ' ' + variant['title']
+                    articulos[0].cantidad = variant['inventory_quantity']
+                    articulos[0].fechaRegistro = datetime.fromisoformat(variant['created_at'])
+                    articulos[0].ultimaFechaActualizacion = datetime.fromisoformat(variant['updated_at'])
+                    articulos[0].save()
+                    
+                    log = LogArticulo.objects.create(
+                        articulo = articulos[0],
+                        json = json.dumps({'articulo': variant})
+                    )
+                    log.save()
+
+    except Exception as e:
+        print(e)
+        return False
     return True
 
 def countProductsFromShopify():
